744e9d51266657317cc864f48eb624c13f021cb9
Emanuele Faggio <faggioemanuele@live.it>
2020-04-01 19:45:35 +0200

    Initial Commit

---
+++ OperationWithCloudKit/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  OperationWithCloudKit
+//
+//  Created by Emanuele Faggio on 01/04/2020.
+//  Copyright © 2020 Emanuele Faggio. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
