2d232bc9843d611b21bbb1b6e629eba5dbc5985f
Emanuele Faggio <faggioemanuele@live.it>
2020-04-02 15:06:29 +0200

    Primo Commit

--- OperationWithCloudKit/ViewController.swift
+++ OperationWithCloudKit/ViewController.swift
@@ -7,14 +7,76 @@
 //
 
 import UIKit
+import CloudKit
+import Foundation
+
 
 class ViewController: UIViewController {
-
+    
+    var titles = [String]()
+    var recordIDs = [CKRecord.ID]()
+    var privateDatabase =  CKContainer.default().privateCloudDatabase
+    
+    @IBOutlet weak var TextField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         // Do any additional setup after loading the view.
     }
-
-
+    
+    
+    @IBAction func saveBtn(_ sender: Any) {
+        let title = TextField.text!
+        let record = CKRecord(recordType: "RecordName")
+        
+        record.setValue(title, forKey: "title")
+        
+        privateDatabase.save(record) { (savedRecord, error) in
+            if error == nil { print("Record saved")  }
+            else { print("Record not saved.. \(error?.localizedDescription)") }
+        }
+    }
+    
+    
+    
+    
+    
+    
+    @IBAction func retrieveBtn(_ sender: Any) {
+        let predicate = NSPredicate(value: true)
+        let query = CKQuery(recordType: "Strings", predicate: predicate)
+        query.sortDescriptors = [NSSortDescriptor(key: "modificationDate", ascending: false)]
+        
+        let operation = CKQueryOperation(query: query)
+        
+        titles.removeAll()
+        recordIDs.removeAll()
+        
+        operation.recordFetchedBlock = { record in
+            self.titles.append(record["title"]!)
+            self.recordIDs.append(record.recordID)
+        }
+        
+        operation.queryCompletionBlock = { cursor, error in
+            
+            DispatchQueue.main.async {
+                
+                print("Titles: \(self.titles)\n")
+                print("RecordIDs: \(self.recordIDs)\n")
+            }
+            
+            
+        }
+        
+        privateDatabase.add(operation)
+    }
+    
+    
+    @IBAction func deleteBtn(_ sender: Any) {
+    }
+    @IBAction func updateBtn(_ sender: Any) {
+    }
+    
 }
 
